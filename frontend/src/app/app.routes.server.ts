ea8494ff8e6b1af14ced9c1adcc7bc73d018d131
Sengtri Bun <sengtri457@gmail.com>
2025-12-13 15:16:38 +0700

    add Files

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,48 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, ServerRoute } from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: "students/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "students/restore/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "students/edit/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "parents/edit/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "parents/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "subjects/edit/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "subjects/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "teachers/edit/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "teachers/:id",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "attendance/stats/:studentId",
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: "**",
+    renderMode: RenderMode.Prerender,
+  },
 ];
